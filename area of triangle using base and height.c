810f863d696ea10f726f4a29ac2a5a208e2d1451
pradiptim <59578079+techpradip@users.noreply.github.com>
2021-05-06 23:00:23 +0545

    Add files via upload

---
+++ area of triangle using base and height.c
@@ -0,0 +1,19 @@
+/*
+Calculating area of triangle by taking base and height as input.
+Formula used Area= 0.5*b *h 
+
+*/
+#include <stdio.h>
+
+int
+main ()
+{
+  int b, h;
+  float area;
+  printf ("Enter base and height respectively:\n");
+  scanf ("%d%d", &b, &h);
+  area = 0.5 * b * h;
+  printf ("Area of triangle = %.2f", area);
+  printf ("\nWritten by techpradip");
+  return 0;
+}
