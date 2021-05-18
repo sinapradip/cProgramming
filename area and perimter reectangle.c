1f55bad240e580c658f2df4d9a62fde0be10f700
pradiptim <59578079+techpradip@users.noreply.github.com>
2021-05-18 10:34:11 +0545

    Add files via upload

---
+++ area and perimter reectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+	float l,b,A,P;
+	printf ("Enter length l and breadth b);
+	scanf("%f%f",&l,&b);
+	A = l * b;
+	P = 2*(l+b);
+	printf ("Area of rectangle is %.2f and perimeter is %.2f",A,P);
+	return 0;
+}
