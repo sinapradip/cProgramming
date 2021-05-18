007a3dc410e9b101829b56aed498d4623e55126f
pradiptim <59578079+techpradip@users.noreply.github.com>
2021-05-18 10:55:53 +0545

    Add files via upload

---
+++ 6.AreaOfCircle.c
@@ -0,0 +1,20 @@
+/*
+C program to calculate area of circle with PI defined.
+
+*/
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+int
+main ()
+{
+  float r, A, rs;
+  printf ("Enter a value of radius: ");
+  scanf ("%f", &r);
+  rs = pow (r, 2);
+  A = PI * rs;
+  printf ("Area of circle of radius %.2f is %.2f", r, A);
+
+  return 0;
+}
