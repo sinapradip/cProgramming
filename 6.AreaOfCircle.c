766e51fc9683dd932f0fb9363225eabeac7195b3
pradiptim <59578079+techpradip@users.noreply.github.com>
2021-05-18 11:05:37 +0545

    Update 6.AreaOfCircle.c

--- 6.AreaOfCircle.c
+++ 6.AreaOfCircle.c
@@ -12,7 +12,7 @@ main ()
   float r, A, rs;
   printf ("Enter a value of radius: ");
   scanf ("%f", &r);
-  rs = pow (r, 2);
+  rs = pow (r, 2); // pow() function makes rs as square of r
   A = PI * rs;
   printf ("Area of circle of radius %.2f is %.2f", r, A);
 
